c56b15da56522f605764cc6b5442ca97f765edbd
Gabe Schenz <gabe.schenz@allergan.com>
2025-04-27 11:28:57 -0500

    format and comments for sql file

--- src/ch09--cloud-entity-resolution-services/Chapter9.sql
+++ src/ch09--cloud-entity-resolution-services/Chapter9.sql
@@ -1,28 +1,65 @@
-# Select all rows with clusters_ids where those cluster_ids have at least one Mari match
+-- TODO: Get a better name for table `clusters_15719257497877843494`.
+-- TODO: Get a better name for table `temp`.
+-- TODO: Be consistent with use of schema name.
+-- TODO: Convert this to use duckdb.
+-- TODO: Rename the file to something more expressive.
 
-CREATE TEMP TABLE temp AS 
-    SELECT src.* FROM Chapter9.clusters_15719257497877843494 AS src
-            INNER JOIN 
-                (SELECT cluster_id from Chapter9.clusters_15719257497877843494 WHERE source_name = "mari") AS mari
-                    ON src.cluster_id = mari.cluster_id;
+-- Select all rows with clusters_ids where those cluster_ids
+-- have at least one Mari match.
+CREATE TEMP TABLE temp
+AS
+WITH mari AS (
+    SELECT cluster_id
+    FROM chapter9.clusters_15719257497877843494
+    WHERE source_name = 'mari'
+)
 
-# Select subset of clusters who have both an Mari match and at least one Basic match. Remove clusters with only Mari match
+SELECT src.*
+FROM chapter9.clusters_15719257497877843494 AS src
+INNER JOIN mari
+    ON (src.cluster_id = mari.cluster_id);
 
-CREATE TEMP TABLE match AS
-    SELECT src.* FROM temp AS src
-        INNER JOIN
-            (SELECT cluster_id FROM temp GROUP BY cluster_id HAVING COUNT(*) > 1) AS matches ON matches.cluster_id = src.cluster_id;
+-- Select subset of clusters who have both an Mari match and at
+-- least one Basic match. Remove clusters with only Mari match
+CREATE TEMP TABLE match
+AS
+WITH matches AS (
+    SELECT cluster_id
+    FROM temp
+    GROUP BY cluster_id
+    HAVING COUNT(*) > 1
+)
 
-# Add extra columns from either basic or mari table, order by cluster for easy comparision
+SELECT src.*
+FROM temp AS src
+INNER JOIN matches
+    ON (src.cluster_id = matches.cluster_id);
 
-CREATE TABLE Chapter9.results AS
-    SELECT * FROM Chapter9.basic AS bas
-        INNER JOIN
-            (SELECT * FROM match WHERE match.source_name = "basic") AS res1        
-                ON res1.source_key = CAST(bas.unique_id AS STRING)
-    UNION ALL
-    SELECT * FROM Chapter9.mari AS mari
-        INNER JOIN
-            (SELECT * FROM match WHERE match.source_name = "mari") AS res2        
-                ON res2.source_key = CAST(mari.unique_id AS STRING)
-    ORDER BY confidence,cluster_id
+-- Add extra columns from either basic or mari table, order by
+-- cluster for easy comparision
+CREATE TABLE chapter9.results
+AS
+WITH res1 AS (
+    SELECT *
+    FROM match
+    WHERE match.source_name = match.basic
+),
+
+res2 AS (
+    SELECT *
+    FROM match
+    WHERE match.source_name = match.mari
+)
+
+SELECT *
+FROM chapter9.basic AS bas
+INNER JOIN res1
+    ON (res1.source_key = CAST(bas.unique_id AS STRING))
+UNION ALL
+SELECT *
+FROM chapter9.mari AS mari
+INNER JOIN res2
+    ON (res2.source_key = CAST(mari.unique_id AS STRING))
+ORDER BY
+    confidence,
+    cluster_id;
